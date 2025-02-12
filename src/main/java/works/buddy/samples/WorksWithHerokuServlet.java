8354b5cfc4113490a2a090096315e41e490fdfac
vinod-o <vinodo3735@gmail.com>
2025-02-12 12:17:17 +0530

    changed

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -16,6 +16,5 @@ public class WorksWithHerokuServlet extends HttpServlet {
         writer.print("Buddy Works with Heroku");
         writer.close();
         /*hello addd */
-        /*hiii */
     }
 }
