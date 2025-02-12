f4e67ab5aff5c1698da3d251c697a18b9af5f3ca
vinod-o <vinodo3735@gmail.com>
2025-02-12 12:06:47 +0530

    creating conflit

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -16,5 +16,6 @@ public class WorksWithHerokuServlet extends HttpServlet {
         writer.print("Buddy Works with Heroku");
         writer.close();
         /*hello addd */
+        /*hiii */
     }
 }
